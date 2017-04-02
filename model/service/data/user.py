feb519998cd4643323d55b8070b21ca1a3596853
ZH1995 <577435302@qq.com>
2017-04-02 16:09:02 +0800

    program register function

--- model/service/data/user.py
+++ model/service/data/user.py
@@ -4,6 +4,8 @@
 # @date   17-4-1
 # @brief
 
+import datetime
+import time
 from model.dao.sql_operation import SQL
 
 class User(object):
@@ -16,13 +18,26 @@ class User(object):
         """
         pass
 
-    def select_uid_by_name_and_pwd(self, username, password):
+    def get_uid_by_name_and_pwd(self, username, password):
         """
-        
+        根据用户名、密码获取用户ID
         :param username: 
         :param password: 
         :return: 
         """
-        sql_sectence = "SELECT uid FROM tblUser WHERE uname=%s and password=%s"
+        sql_sentence = "SELECT uid FROM tblUser WHERE uname=%s and password=%s"
         param_list = (username, password)
-        return SQL().fetch_one(sql_sectence, param_list)
+        return SQL().fetch_one(sql_sentence, param_list)
+
+    def add_new_user(self, username, password, headimg = ""):
+        """
+        
+        :param username: 
+        :param password: 
+        :param headimg: 
+        :return: 
+        """
+        sql_sentence = "INSERT INTO tblUser (`uname`, `password`, `head_img`, `create_time`) VALUES (%s, %s, %s, %s)"
+        create_time = int(time.mktime(datetime.datetime.now().timetuple()))
+        param_list = (username, password, headimg, create_time)
+        return SQL().execute_one(sql_sentence, param_list)
