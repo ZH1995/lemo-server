03e999ded7bd228a5fa89b4a5fcf9b805a0a3ce0
ZH1995 <577435302@qq.com>
2017-04-02 16:13:55 +0800

    add register page

---
+++ model/service/page/register.py
@@ -0,0 +1,46 @@
+# -*- coding: UTF-8 -*-
+# @file   register
+# @author zh1995
+# @date   17-4-1
+# @brief
+
+from base_page import BasePage
+from model.service.data.user import User
+
+
+class Register(BasePage):
+    """
+    注册类
+    """
+
+    def _check_param(self):
+        """
+        参数校验
+        :return: 
+        """
+        if self._get_param("username") is None:
+            raise Exception("User name is invalid")
+        if self._get_param("password") is None:
+            raise Exception("Password is invalid")
+
+    def _execute(self, req):
+        """
+        执行函数
+        :param req: 
+        :return: 
+        """
+        username = str(self._get_param("username"))
+        password = str(self._get_param("password"))
+        create_user_res = User().add_new_user(username, password)
+        if create_user_res is None:
+            return {
+                "errno": -1,
+                "errmsg": "create account fail",
+            }
+
+        uid = User().get_uid_by_name_and_pwd(username, password)
+        return {
+            "data": {
+                "uid": uid[0]
+            }
+        }
